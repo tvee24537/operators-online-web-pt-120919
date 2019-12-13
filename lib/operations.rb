5f995c7153f58aacfdf26df573bc8d0c1114f410
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-13 05:06:39 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-  if speed < 40 or speed > 60
-    return 
+  if speed < 40 && speed > 60
+    return true
+  else speed 
 end
 
 
 
 def not_safe?(speed)
-	speed < 40 or speed > 60
+	speed < 40 or speed > 60 ? true : false
 end
 	
 
